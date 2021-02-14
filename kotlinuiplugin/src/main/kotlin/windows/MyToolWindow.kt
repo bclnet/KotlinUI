4eedfbf2fc61a851fc5a90faf4b9e0a24a1195e1
Sky Morey <smorey@degdigital.com>
2021-02-14 15:26:09 -0600

    updates

--- kotlinuiplugin/src/main/kotlin/windows/MyToolWindow.kt
+++ kotlinuiplugin/src/main/kotlin/windows/MyToolWindow.kt
@@ -10,38 +10,38 @@ import java.awt.event.ActionEvent
 import javax.swing.JLabel
 
 class MyToolWindow(toolWindow: ToolWindow) {
-    private var refreshToolWindowButton: JButton? = null
-    private var hideToolWindowButton: JButton? = null
-    private var currentDate: JLabel? = null
-    private var currentTime: JLabel? = null
-    private var timeZone: JLabel? = null
-    private var myToolWindowContent: JPanel? = null
+    private lateinit var refreshToolWindowButton: JButton
+    private lateinit var hideToolWindowButton: JButton
+    private lateinit var currentDate: JLabel
+    private lateinit var currentTime: JLabel
+    private lateinit var timeZone: JLabel
+    private lateinit var myToolWindowContent: JPanel
 
     init {
-        hideToolWindowButton!!.addActionListener { e: ActionEvent? -> toolWindow.hide(null) }
-        refreshToolWindowButton!!.addActionListener { e: ActionEvent? -> currentDateTime() }
+        hideToolWindowButton.addActionListener { e: ActionEvent? -> toolWindow.hide(null) }
+        refreshToolWindowButton.addActionListener { e: ActionEvent? -> currentDateTime() }
         currentDateTime()
     }
 
     fun currentDateTime() {
         // Get current date and time
         val instance = Calendar.getInstance()
-        currentDate!!.text = (instance[Calendar.DAY_OF_MONTH].toString() + "/"
+        currentDate.text = (instance[Calendar.DAY_OF_MONTH].toString() + "/"
                 + (instance[Calendar.MONTH] + 1) + "/"
                 + instance[Calendar.YEAR])
-        currentDate!!.icon = ImageIcon(javaClass.getResource("/windows/Calendar-icon.png"))
+        currentDate.icon = ImageIcon(javaClass.getResource("/windows/Calendar-icon.png"))
         val min = instance[Calendar.MINUTE]
         val strMin = if (min < 10) "0$min" else min.toString()
-        currentTime!!.text = instance[Calendar.HOUR_OF_DAY].toString() + ":" + strMin
-        currentTime!!.icon = ImageIcon(javaClass.getResource("/windows/Time-icon.png"))
+        currentTime.text = instance[Calendar.HOUR_OF_DAY].toString() + ":" + strMin
+        currentTime.icon = ImageIcon(javaClass.getResource("/windows/Time-icon.png"))
         // Get time zone
         val gmt_Offset = instance[Calendar.ZONE_OFFSET].toLong() // offset from GMT in milliseconds
         var str_gmt_Offset = (gmt_Offset / 3600000).toString()
         str_gmt_Offset = if (gmt_Offset > 0) "GMT + $str_gmt_Offset" else "GMT - $str_gmt_Offset"
-        timeZone!!.text = str_gmt_Offset
-        timeZone!!.icon = ImageIcon(javaClass.getResource("/windows/Time-zone-icon.png"))
+        timeZone.text = str_gmt_Offset
+        timeZone.icon = ImageIcon(javaClass.getResource("/windows/Time-zone-icon.png"))
     }
 
-    val content: JPanel? =
+    val content: JPanel =
         myToolWindowContent
 }
